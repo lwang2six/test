53a4a998b812d4357ebf55264959832500a9fcb3
Leo Wang <lwang2six@gmail.com>
2011-03-09 21:11:45 -0800

    hello

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wow::Application.config.secret_token = '3c9cdec9d3664912cdf9f50dfbd2e132aa47cc0ad9509950d3fa3dd11462ea308ca8e9687d4dcb1e23f7e1c989d3015352cb43ec27d461dfee288a6cd81cf7d6'
